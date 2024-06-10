ec117a469efb324d6820125602d1310a1432dd7e
tkDev <153438920+tkDev0101@users.noreply.github.com>
2024-06-10 16:40:54 +0200

    updated code

--- Note-App-Realtime-Database/app/build.gradle.kts
+++ Note-App-Realtime-Database/app/build.gradle.kts
@@ -47,10 +47,63 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.firebase:firebase-auth:23.0.0")
+
+
+
+    // FirebaseUI for Realtime Database
+    implementation("com.firebaseui:firebase-ui-database:8.0.0")
+
+    // FirebaseUI for Firebase Auth
+    implementation("com.firebaseui:firebase-ui-auth:8.0.0")
+
+    // FirebaseUI for Cloud Storage
+    implementation("com.firebaseui:firebase-ui-storage:8.0.0")
+
+    // Firebase BoM (Bill of Materials)
+    implementation("com.google.firebase:firebase-bom:29.0.4")
+
+    // Firebase Authentication library (ktx version)
+    implementation("com.google.firebase:firebase-auth-ktx:21.0.0")
+
+    // Firebase Realtime Database
     implementation("com.google.firebase:firebase-database:21.0.0")
+
+    // Firebase Cloud Storage
     implementation("com.google.firebase:firebase-storage:21.0.0")
 
+    // Firebase Firestore
+    implementation("com.google.firebase:firebase-firestore:25.0.0") // Update version
+
+    // Declare the dependency for the Google Play services library and specify its version
+    implementation("com.google.android.gms:play-services-auth:20.0.1")
+
+    // For Kotlin coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.1.1")
+
+
+
+
+
+
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+/*
+original dependencies
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+
+///*
 
     // FirebaseUI for Realtime Database
     implementation("com.firebaseui:firebase-ui-database:8.0.0")
@@ -82,6 +135,33 @@ dependencies {
     // For Kotlin coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.1.1")
+    implementation("com.google.firebase:firebase-auth:23.0.0")
+    implementation("com.google.firebase:firebase-firestore:25.0.0")
+
+//*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -90,3 +170,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
+*/
+
+
--- Note-App-Realtime-Database/app/src/main/java/com/example/note_app_realtime_database/Note.kt
+++ Note-App-Realtime-Database/app/src/main/java/com/example/note_app_realtime_database/Note.kt
@@ -1,6 +1,7 @@
 package com.example.note_app_realtime_database
 
 data class Note (
+        val noteId: String? = "",
         val text: String = "",
         val uid: String =""
     )
