a8d8d146ffa86849d56785acfe1542527b324ba5
tkDev <153438920+tkDev0101@users.noreply.github.com>
2024-06-10 13:11:55 +0200

    Created another Note app using RealTime Database

---
+++ Note-App-Realtime-Database/app/src/main/java/com/example/note_app_realtime_database/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.note_app_realtime_database
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
--- NoteAppnoSignIn/app/src/main/java/com/example/note_app_no_signin/MainActivity.kt
+++ NoteAppnoSignIn/app/src/main/java/com/example/note_app_no_signin/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
@@ -16,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     //Variables
     private lateinit var recyclerView: RecyclerView
     private lateinit var fab: FloatingActionButton
+    private lateinit var backFAB: FloatingActionButton
     private lateinit var noteDao: NoteDao
     private lateinit var auth: FirebaseAuth
     private lateinit var adaptor: RVAdaptor
@@ -27,6 +29,7 @@ class MainActivity : AppCompatActivity() {
         //TYPECASTING
         recyclerView = findViewById(R.id.recycleView)
         fab = findViewById(R.id.fab)
+        backFAB = findViewById(R.id.backFAB)
 
         noteDao = NoteDao()
         auth = FirebaseAuth.getInstance()   //auth = Firebase.auth
@@ -35,6 +38,14 @@ class MainActivity : AppCompatActivity() {
         fab.setOnClickListener{
             //naviaget to new activity to add notes
             val intent = Intent(this, AddNoteActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        //Event Handler -> onClickListener
+        backFAB.setOnClickListener{
+            //naviaget to new activity to add notes
+            val intent = Intent(this, Login::class.java)
             startActivity(intent)
         }
 
@@ -65,6 +76,23 @@ class MainActivity : AppCompatActivity() {
         adaptor = RVAdaptor(recyclerViewOption)
         recyclerView.adapter = adaptor
 
+        //create itemTouchHelper to swipe to delete note
+
+    ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT){
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ): Boolean {
+            return false
+        }
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            val position = viewHolder.absoluteAdapterPosition
+            adaptor.deleteNote(position)
+        }
+
+    }).attachToRecyclerView(recyclerView)
 
     }
 
